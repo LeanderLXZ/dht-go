6da40f6e502ae6278c31e37c7582a1cb9f2a0d6f
Xuzheng Lu <xuzheng_lu@gwu.edu>
2020-12-16 01:36:09 -0500

    Add rpc prototype

--- src/node.go
+++ src/node.go
@@ -1,12 +1,13 @@
 package src
 
 import (
+	"crypto/sha1"
 	"hash"
 	"time"
-
 	"google.golang.org/grpc"
 )
 
+// Structure for parameters
 type Parameters struct {
 	NodeID        string
 	Address       string
@@ -16,6 +17,24 @@ type Parameters struct {
 	MaxIdleTime   time.Duration    // Maximum idle time
 	MinStableTime time.Duration    // Minimum stable time
 	MaxStableTime time.Duration    // Maximum stable time
-	ServerOption  []grpc.ServerOption
-	DialOption    []grpc.DialOption
+	ServerOptions []grpc.ServerOption
+	DialOptions   []grpc.DialOption
 }
+
+// Get a default parameters settings
+func GetDefaultParameters() *Parameters {
+	param := Parameters{}
+	param.HashFunc = sha1.New
+	param.HashLen = param.HashFunc.Size() * 8
+	param.DialOptions = make([grpc.DialOption, 0, 5])
+	param.DialOptions = append(
+		param.DialOptions,
+		grpc.WithBloc(),
+		grpc.WithTimeout(5*time.Second),
+		grpc.FailOnNonTempDialError(true),
+		grpc.WithInsecure()
+	)
+	return param
+}
+
+
