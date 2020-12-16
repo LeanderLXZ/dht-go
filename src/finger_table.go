aaae8586bccee132992e19db698c519599185e2e
RyanShen <781682916@qq.com>
2020-12-16 17:10:40 -0500

    3rd update finger

--- src/finger_table.go
+++ src/finger_table.go
@@ -1,10 +1,8 @@
-package chord
+package src
 
 import (
 	"fmt"
 	"math/big"
-
-	"github.com/project-schrodinger/models" //这个models里的内容我们还没加在框架里，后面需要加
 )
 
 type fingerTable []*fingerEntry
@@ -32,15 +30,15 @@ func newFingerEntry(initId []byte, remoteNode *models.Node) *fingerEntry {
 
 func (node *Node) findNextFinger(next int) int {
 	nextHash := FingerMath(node.Id, next, n.cnf.HashSize)
-	succ, err := node.findSuccessor(nextHash)
+	nextOne, errors := node.findSuccessor(nextHash)
 	nextNum := (next + 1) % node.cnf.HashSize
-	if err != nil || succ == nil {
-		fmt.Println("error: ", err, succ)
+	if err != nil || nextOne == nil {
+		fmt.Println("error: ", errors, nextOne)
 		fmt.Printf("finger lookup failed %x %x \n", node.Id, nextHash)
 		return nextNum
 	}
 
-	finger := newFingerEntry(nextHash, succ)
+	finger := newFingerEntry(nextHash, nextOne)
 	node.ftMtx.Lock()
 	node.fingerTable[next] = finger
 
@@ -55,17 +53,17 @@ func FingerMath(node []byte, i int, m int) []byte {
 	idInt := (&big.Int{}).SetBytes(node)
 
 	// Get the offset
-	two := big.NewInt(2)
-	offset := big.Int{}
-	offset.Exp(two, big.NewInt(int64(i)), nil)
+	twoOffset := big.NewInt(2)
+	newOffset := big.Int{}
+	newOffset.Exp(twoOffset, big.NewInt(int64(i)), nil)
 
 	// Sum
 	sum := big.Int{}
-	sum.Add(idInt, &offset)
+	sum.Add(idInt, &newOffset)
 
 	// Get the ceiling
 	ceil := big.Int{}
-	ceil.Exp(two, big.NewInt(int64(m)), nil)
+	ceil.Exp(twoOffset, big.NewInt(int64(m)), nil)
 
 	// Apply the mod
 	idInt.Mod(&sum, &ceil)
