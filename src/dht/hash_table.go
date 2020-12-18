06b541414224e13153487bad6bc9af960d2c4b92
Xuzheng Lu <xuzheng_lu@gwu.edu>
2020-12-17 21:37:00 -0500

    Add interactive nodes

---
+++ src/dht/hash_table.go
@@ -0,0 +1,77 @@
+package dht
+
+import (
+	"hash"
+)
+
+type hash_table interface {
+	GetValue(string) ([]byte, error)
+	AddKey(string, string) error
+	DeleteKey(string) error
+	Between([]byte, []byte) ([]*KeyValuePair, error)
+	DeleteKeys(...string) error
+}
+
+type DataHash struct {
+	data map[string]string
+	Hash func() hash.Hash
+}
+
+func NewDataHash(hashFunc func() hash.Hash) hash_table {
+	return &DataHash{
+		data: make(map[string]string),
+		Hash: hashFunc,
+	}
+}
+
+func (dh *DataHash) hashKey(key string) ([]byte, error) {
+	h := dh.Hash()
+	if _, err := h.Write([]byte(key)); err != nil {
+		return nil, err
+	}
+	val := h.Sum(nil)
+	return val, nil
+}
+
+func (dh *DataHash) AddKey(key, value string) error {
+	dh.data[key] = value
+	return nil
+}
+
+func (dh *DataHash) GetValue(key string) ([]byte, error) {
+	value, res := dh.data[key]
+	if !res {
+		return nil, ERR_KEY_NOT_FOUND
+	}
+	return []byte(value), nil
+}
+
+func (dh *DataHash) DeleteKey(key string) error {
+	delete(dh.data, key)
+	return nil
+}
+
+func (dh *DataHash) DeleteKeys(keys ...string) error {
+	for _, k := range keys {
+		delete(dh.data, k)
+	}
+	return nil
+}
+
+func (dh *DataHash) Between(from []byte, to []byte) ([]*KeyValuePair, error) {
+	values := make([]*KeyValuePair, 0, 10)
+	for k, v := range dh.data {
+		hashedKey, err := dh.hashKey(k)
+		if err != nil {
+			continue
+		}
+		if betweenRightIncl(hashedKey, from, to) {
+			pair := &KeyValuePair{
+				Key:   k,
+				Value: v,
+			}
+			values = append(values, pair)
+		}
+	}
+	return values, nil
+}
---
+++ src/dht/node_instances/interactive_node1.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"dht"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+)
+
+func createNode(id string, addr string, sister *dht.NodeRPC) (*dht.Node, error) {
+
+	p := dht.GetInitialParameters()
+	p.NodeId = id
+	p.Address = addr
+	p.Timeout = 10 * time.Millisecond
+	p.MaxIdleTime = 1000 * time.Millisecond
+
+	n, err := dht.CreateNode(p, sister)
+	return n, err
+}
+
+func main() {
+	id1 := "1"
+	sister := dht.CreateNodeById(id1, "0.0.0.0:8001")
+
+	h, err := createNode("12", "0.0.0.0:8004", sister)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	input := bufio.NewScanner(os.Stdin)
+	time.Sleep(4000 * time.Millisecond)
+	fmt.Println("----------------------------------------------------------------")
+	fmt.Print("Input: ")
+	go func() {
+		for input.Scan() {
+			line := input.Text()
+			array := strings.Fields(line)
+
+			if line == "q" {
+				break
+			}
+			if array[0] == "add" {
+				if len(array) != 3 {
+					fmt.Println("Incorrect input! Please input again.")
+					goto JUMPPOINT
+				}
+				sErr := h.AddKey(array[1], array[2])
+				if sErr != nil {
+					log.Println("err: ", sErr)
+				}
+			} else if array[0] == "del" {
+				if len(array) != 1 {
+					fmt.Println("Incorrect input! Please input again.")
+					goto JUMPPOINT
+				}
+				err := h.DeleteKey(array[1])
+				if err != nil {
+					log.Println("err: ", err)
+				}
+			} else if array[0] == "get" {
+				if len(array) != 2 {
+					fmt.Println("Incorrect input! Please input again.")
+					goto JUMPPOINT
+				}
+				val, err := h.GetValue(array[1])
+				if err != nil {
+					log.Println("err", err)
+				}
+				fmt.Printf("Output: ")
+				if val != nil {
+					fmt.Printf("%s\n", string(val))
+				} else {
+					fmt.Printf("Key not found\n")
+				}
+			} else if array[0] == "getloc" {
+				if len(array) != 2 {
+					fmt.Println("Incorrect input! Please input again.")
+					goto JUMPPOINT
+				}
+				node, err := h.GetLocation(array[1])
+				if err != nil {
+					log.Println("err: ", err)
+				}
+				fmt.Printf("Output: ")
+				if node != nil {
+					fmt.Printf("%d\n", (&big.Int{}).SetBytes(node.NodeId))
+				} else {
+					fmt.Printf("Key not found\n")
+				}
+			} else if array[0] == "ft" {
+				if len(array) != 1 {
+					fmt.Println("Incorrect input! Please input again.")
+					goto JUMPPOINT
+				}
+				fts := h.GetFingerTable()
+				if fts != nil {
+					fmt.Printf("Output: \n")
+					for i := 0; i < len(fts); i++ {
+						ft := fts[i]
+						fmt.Printf(
+							"ID: %d | Remote Node ID: %d\n",
+							(&big.Int{}).SetBytes(ft.InitId),
+							(&big.Int{}).SetBytes(ft.RemoteNode.NodeId),
+						)
+					}
+					fmt.Println("-----------------------------")
+					fmt.Printf("Length of finger table: %d\n", len(fts))
+				}
+			} else {
+				fmt.Println("Incorrect input! Please input again.")
+			}
+		JUMPPOINT:
+			fmt.Println("----------------------------------------------------------------")
+			fmt.Print("Input: ")
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	h.Stop()
+
+}
